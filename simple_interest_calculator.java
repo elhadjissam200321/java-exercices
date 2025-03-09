c16f61f7ae43230a0541a6faaed0312b8939968d
ELHADJ ISSAM <elhadjissam200321@gmail.com>
2025-03-09 02:23:13 +0000

    simple_interest_calculator.java

---
+++ simple_interest_calculator.java
@@ -0,0 +1,21 @@
+package com.mycompany.first_project;
+import java.util.Scanner;
+
+public class Simple_interest_calculator{
+   public static void main(String[] args){
+          Scanner s=new Scanner(System.in);
+          double principal,rate,time,interest;
+          System.out.println("Enter principal amount :");
+          principal=s.nextDouble();
+          System.out.println("Enter rate of interest :");
+          rate=s.nextDouble();
+          System.out.println("Enter Time in years");
+          time=s.nextDouble();
+          interest=(principal*rate*time)/100;
+          System.out.println("Interest is :"+interest);
+          
+             
+
+   }
+
+}
