48d528fe744e21491a132c9816d6640ce4e5c888
ELHADJ ISSAM <elhadjissam200321@gmail.com>
2025-03-09 02:18:38 +0000

    age_calculation.java

---
+++ age_calculation.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Age_calculation{
+   public static void main(String[] args){
+          Scanner s=new Scanner(System.in);
+          int birth_year;
+          System.out.println("Enter birth year :");
+          birth_year=s.nextInt();
+          System.out.println("Your age is :" +(2025-birth_year));
+          
+            
+
+   }
+
+}
