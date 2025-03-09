faf807a267975bc355f28ab2ed8e30527f063044
ELHADJ ISSAM <elhadjissam200321@gmail.com>
2025-03-09 01:57:33 +0000

    greeting_message.java
    Greeting message

---
+++ greeting_message.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Greeting_message{
+   public static void main(String[] args){
+      String name;
+          Scanner s=new Scanner(System.in);
+System.out.println("Enter your name: ");
+      name=s.nextLine();
+      System.out.println("Hello "+name+", welcome to Java programming!");
+
+   }
+
+}
